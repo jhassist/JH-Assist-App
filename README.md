13d2c36108c46b8a9d6cfd9b0778ab4f0b8d6638
jhassist <schulz-alsen@t-online.de>
2026-07-15 12:06:21 +0200

    Version 1.0 RC4 – Datentransfer PC und iPhone

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # JH Assist Web-App
 
 Dieser Ordner enthält ausschließlich die statische iPhone-/Browser-App. Es sind keine Klientendaten, Sicherungen, Excel-Vorlagen oder Exportprogramme enthalten.
+
+
+RC4 enthält den getrennten Stammdaten- und Leistungstransfer zwischen PC und iPhone.
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='jh-assist-1.0-rc3-v1';
+const CACHE_NAME='jh-assist-1.0-rc4-v1';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()));
